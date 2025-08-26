5db00d259be31d25d48a26b9eaba1ed0b28abe82
Abhishek Prasad <prasadabhishek550@gmail.com>
2025-08-27 00:04:49 +0530

    setup app.js file

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    return res.json({ message: 'Welcome to Ecommerce API' });
+})
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server is up and running on port 🏃 ${PORT}`));
